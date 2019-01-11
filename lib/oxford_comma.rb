5d4d20b7593251bc20fe9474a0e2fcfab4b98521
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-11 02:57:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.size == 1 
     array.join
-  else array.size == 2 
+  elsif array.size == 2 
     array.join (" and ")
+  else 
+    array.join (" , ")
   end
 end
